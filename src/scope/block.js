04d5d8fd4c84628a610a8ad9471a76a42e02da2d
santiagobr <santiagobastidasrodriguez@gmail.com>
2021-08-16 19:00:05 -0500

    fourth class: block scope

---
+++ src/scope/block.js
@@ -0,0 +1,67 @@
+// const fruits = () => {
+//   if (true) {
+//     var fruits1 = 'apple';
+//     let fruits2 = 'banana';
+//     const fruits3 = 'kiwi';
+//   }
+
+//   console.log(fruits1)
+//   console.log(fruits2) --> ReferenceError: fruits2 is not defined 
+//   console.log(fruits3) --> ReferenceError: fruits3 is not defined
+// }
+
+// fruits()
+
+
+const fruits = () => {
+  if (true) {
+    var fruits1 = 'apple';
+    let fruits2 = 'banana';
+    const fruits3 = 'kiwi';
+
+    console.log(fruits2)  
+    console.log(fruits3) 
+  }
+
+  console.log(fruits1)
+}
+
+fruits()
+
+
+let x = 1 
+{
+  let x =  2
+  console.log(x)
+}
+
+console.log(x)
+
+var x = 1 
+{
+  var x =  2
+  console.log(x)
+}
+
+console.log(x)
+
+
+const anotherFunction = () => {
+  for(let i = 0; i < 10; i++) {
+    setTimeout(() => {
+      console.log(i)
+    }, 1000)
+  }
+}
+
+anotherFunction()
+
+// const anotherFunction = () => {
+//   for(var i = 0; i < 10; i++) {
+//     setTimeout(() => {
+//       console.log(i)
+//     }, 1000)
+//   }
+// }
+
+// anotherFunction()
